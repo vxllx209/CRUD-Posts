a1f19888fa1ef4bed9c39f92ec46a8bd7ea02954
vxllx209 <bvillagrancalderon@gmail.com>
2026-09-21 09:34:28 -0300

    Primeros pasos

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CRUD-Posts
 Pagina Inicial para aprender sobre CRUD
+
+
+// Pasos para my sql
+
+1- Instalar MYSQL2
+
+2- Crae DB
+
+3- Crear schema.sql
+
+4- Preparar .env 
+
+5- 
---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+});
+
+module.exports = pool;      
